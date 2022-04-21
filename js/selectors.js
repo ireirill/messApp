7c95f3ff56853f4e59919db6a87c6dd73318dc51
Irene <ireiri@gmail.com>
2022-04-21 17:57:40 +0200

    User can add messages to a channel

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,7 @@
 getNewChannelInput = () => document.getElementById("new-channel-input");
 getNewChannelModal = () => document.getElementById("new-channel-modal");
 getChannelList = () => document.getElementById("channels-list");
+getNewMessageButton = () => document.getElementById("new-message-button");
+getNewMessageInput = () => document.getElementById("message-input");
+getChatTitleText = () => document.getElementById("chat-title-text");
+getChatMessages = () => document.getElementById("chat-messages");
